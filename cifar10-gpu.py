23d55f39895c40163a037d3d885ca059ae463d95
Kaushik Roy <kaushik.roy1984@gmail.com>
2024-06-06 21:26:48 -0400

    Create cifar10-gpu.py

---
+++ cifar10-gpu.py
@@ -0,0 +1,85 @@
+import numpy as np
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.datasets import CIFAR10
+from torch.utils.data import DataLoader
+
+import argparse
+
+parser = argparse.ArgumentParser(description='cifar10 classification models, single gpu performance test')
+parser.add_argument('--lr', default=0.1, help='')
+parser.add_argument('--batch_size', type=int, default=512, help='')
+parser.add_argument('--num_workers', type=int, default=0, help='')
+
+
+def main():
+
+    args = parser.parse_args()
+
+    class Net(nn.Module):
+
+       def __init__(self):
+          super(Net, self).__init__()
+
+          self.conv1 = nn.Conv2d(3, 6, 5)
+          self.pool = nn.MaxPool2d(2, 2)
+          self.conv2 = nn.Conv2d(6, 16, 5)
+          self.fc1 = nn.Linear(16 * 5 * 5, 120)
+          self.fc2 = nn.Linear(120, 84)
+          self.fc3 = nn.Linear(84, 10)
+
+       def forward(self, x):
+          x = self.pool(F.relu(self.conv1(x)))
+          x = self.pool(F.relu(self.conv2(x)))
+          x = x.view(-1, 16 * 5 * 5)
+          x = F.relu(self.fc1(x))
+          x = F.relu(self.fc2(x))
+          x = self.fc3(x)
+          return x
+
+    net = Net().cuda() # Load model on the GPU
+
+    criterion = nn.CrossEntropyLoss().cuda() # Load the loss function on the GPU
+    optimizer = optim.SGD(net.parameters(), lr=args.lr)
+
+    transform_train = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    dataset_train = CIFAR10(root='./data', train=True, download=False, transform=transform_train)
+
+    train_loader = DataLoader(dataset_train, batch_size=args.batch_size, num_workers=args.num_workers)
+
+    perf = []
+
+    total_start = time.time()
+
+    for batch_idx, (inputs, targets) in enumerate(train_loader):
+
+       start = time.time()
+       
+       inputs = inputs.cuda() 
+       targets = targets.cuda()
+
+       outputs = net(inputs)
+       loss = criterion(outputs, targets)
+
+       optimizer.zero_grad()
+       loss.backward()
+       optimizer.step()
+
+       batch_time = time.time() - start
+
+       images_per_sec = args.batch_size/batch_time
+
+       perf.append(images_per_sec)
+
+    total_time = time.time() - total_start
+
+if __name__=='__main__':
+   main()
