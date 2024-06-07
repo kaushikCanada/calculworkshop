d1ff906700ce37023aab99c050585f9704ffdb6c
Kaushik Roy <kaushik.roy1984@gmail.com>
2024-06-06 23:13:09 -0400

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --nodes 2
-#SBATCH --gres=gpu:2 # request a GPU
-#SBATCH --tasks-per-node=2
+#SBATCH --nodes 1
+#SBATCH --gres=gpu:1 # request a GPU
+#SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=8G
 #SBATCH --time=0-01:00:00
