0e6a44dcd42dbc0f682a0adbd9855613f667374e
Kaushik Roy <kaushik.roy1984@gmail.com>
2024-06-21 13:39:03 -0400

    Update cifar10.py

--- cifar10.py
+++ cifar10.py
@@ -99,33 +99,45 @@ def main():
 
     class Net(pl.LightningModule):
 
-       def __init__(self):
-          super(Net, self).__init__()
+        def __init__(self):
+            super(Net, self).__init__()
 
-          self.conv1 = nn.Conv2d(3, 6, 5)
-          self.pool = nn.MaxPool2d(2, 2)
-          self.conv2 = nn.Conv2d(6, 16, 5)
-          self.fc1 = nn.Linear(16 * 5 * 5, 120)
-          self.fc2 = nn.Linear(120, 84)
-          self.fc3 = nn.Linear(84, 10)
+            self.conv1 = nn.Conv2d(3, 6, 5)
+            self.pool = nn.MaxPool2d(2, 2)
+            self.conv2 = nn.Conv2d(6, 16, 5)
+            self.fc1 = nn.Linear(16 * 5 * 5, 120)
+            self.fc2 = nn.Linear(120, 84)
+            self.fc3 = nn.Linear(84, 10)
 
-       def forward(self, x):
-          x = self.pool(F.relu(self.conv1(x)))
-          x = self.pool(F.relu(self.conv2(x)))
-          x = x.view(-1, 16 * 5 * 5)
-          x = F.relu(self.fc1(x))
-          x = F.relu(self.fc2(x))
-          x = self.fc3(x)
-          return x
+        def forward(self, x):
+            x = self.pool(F.relu(self.conv1(x)))
+            x = self.pool(F.relu(self.conv2(x)))
+            x = x.view(-1, 16 * 5 * 5)
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = self.fc3(x)
+            return x
 
-       def training_step(self, batch, batch_idx):
-          x, y = batch
-          y_hat = self(x)
-          loss = F.cross_entropy(y_hat, y)
-          return loss
+        def training_step(self, batch, batch_idx):
+            x, y = batch
+            y_hat = self(x)
+            loss = F.cross_entropy(y_hat, y)
+            return loss
 
-       def configure_optimizers(self):
-          return torch.optim.Adam(self.parameters(), lr=float(args.lr))
+        def validaiton_step(self, batch, batch_idx):
+            x, y = batch
+            y_hat = self(x)
+            loss = F.cross_entropy(y_hat, y)
+            return loss
+
+        def test_step(self, batch, batch_idx):
+            x, y = batch
+            y_hat = self(x)
+            loss = F.cross_entropy(y_hat, y)
+            return loss
+
+        def configure_optimizers(self):
+            return torch.optim.Adam(self.parameters(), lr=float(args.lr))
 
     trainer = pl.Trainer(accelerator="gpu", devices=2, num_nodes=2, sync_batchnorm=True,
                         use_distributed_sampler=True, max_epochs = args.max_epochs,
